6614f8e57455810b4148c76e23e5db6d13cf9179
nefer@l2jfrozen.com <nefer@l2jfrozen.com@381c0624-fcce-11e6-9a91-d1aaf0ac2679>
2017-04-11 08:19:33 +0000

    [CORE] - Fixed IllegalArgumentException: No enum for stat/stats command. - Added config for enable/disable vote commands. - Added missing check to load .stat and .stats command.

--- gameserver/branches/cleaning/game-src/com/l2jfrozen/gameserver/handler/voicedcommandhandlers/StatsCmd.java
+++ gameserver/branches/cleaning/game-src/com/l2jfrozen/gameserver/handler/voicedcommandhandlers/StatsCmd.java
@@ -35,174 +35,153 @@ public class StatsCmd implements IVoicedCommandHandler
 		"stats"
 	};
 	
-	private enum CommandEnum
-	{
-		stat,
-		stats
-	}
-	
 	@Override
 	public boolean useVoicedCommand(final String command, final L2PcInstance activeChar, final String target)
 	{
-		final CommandEnum comm = CommandEnum.valueOf(command);
-		
-		if (comm == null)
-			return false;
-		
-		switch (comm)
+		if (command.equalsIgnoreCase("stat"))
 		{
-			case stat:
+			if (!Config.ALLOW_DETAILED_STATS_VIEW)
 			{
-				
-				if (!Config.ALLOW_DETAILED_STATS_VIEW)
-				{
-					return false;
-				}
-				
-				if (activeChar.getTarget() == null)
-				{
-					activeChar.sendMessage("You have no one targeted.");
-					return false;
-				}
-				if (activeChar.getTarget() == activeChar)
-				{
-					activeChar.sendMessage("You cannot request your stats.");
-					return false;
-				}
-				
-				if (!(activeChar.getTarget() instanceof L2PcInstance))
-				{
-					activeChar.sendMessage("You can only get the info of a player.");
-					return false;
-				}
-				
+				return false;
+			}
+			
+			if (activeChar.getTarget() == null)
+			{
+				activeChar.sendMessage("You have no one targeted.");
+				return false;
+			}
+			if (activeChar.getTarget() == activeChar)
+			{
+				activeChar.sendMessage("You cannot request your stats.");
+				return false;
+			}
+			
+			if (!(activeChar.getTarget() instanceof L2PcInstance))
+			{
+				activeChar.sendMessage("You can only get the info of a player.");
+				return false;
+			}
+			
+			NpcHtmlMessage adminReply = new NpcHtmlMessage(5);
+			L2PcInstance targetp = (L2PcInstance) activeChar.getTarget();
+			
+			TextBuilder replyMSG = new TextBuilder("<html><body><center>");
+			replyMSG.append("<br><br><font color=\"00FF00\">=========>>" + targetp.getName() + "<<=========</font><br>");
+			replyMSG.append("<font color=\"FF0000\">Level: " + targetp.getLevel() + "</font><br>");
+			
+			if (targetp.getClan() != null)
+			{
+				replyMSG.append("<font color=\"FF0000\">Clan: " + targetp.getClan().getName() + "</font><br>");
+				replyMSG.append("<font color=\"FF0000\">Alliance: " + targetp.getClan().getAllyName() + "</font><br>");
+			}
+			else
+			{
+				replyMSG.append("<font color=\"FF0000\">Alliance: None</font><br>");
+				replyMSG.append("<font color=\"FF0000\">Clan: None</font><br>");
+			}
+			
+			replyMSG.append("<font color=\"FF0000\">Adena: " + targetp.getAdena() + "</font><br>");
+			
+			if (targetp.getInventory().getItemByItemId(6393) == null)
+			{
+				replyMSG.append("<font color=\"FF0000\">Medals : 0</font><br>");
+			}
+			else
+			{
+				replyMSG.append("<font color=\"FF0000\">Medals : " + targetp.getInventory().getItemByItemId(6393).getCount() + "</font><br>");
+			}
+			
+			if (targetp.getInventory().getItemByItemId(3470) == null)
+			{
+				replyMSG.append("<font color=\"FF0000\">Gold Bars : 0</font><br>");
+			}
+			else
+			{
+				replyMSG.append("<font color=\"FF0000\">Gold Bars : " + targetp.getInventory().getItemByItemId(3470).getCount() + "</font><br>");
+			}
+			
+			replyMSG.append("<font color=\"FF0000\">PvP Kills: " + targetp.getPvpKills() + "</font><br>");
+			replyMSG.append("<font color=\"FF0000\">PvP Flags: " + targetp.getPvpFlag() + "</font><br>");
+			replyMSG.append("<font color=\"FF0000\">PK Kills: " + targetp.getPkKills() + "</font><br>");
+			replyMSG.append("<font color=\"FF0000\">HP, CP, MP: " + targetp.getMaxHp() + ", " + targetp.getMaxCp() + ", " + targetp.getMaxMp() + "</font><br>");
+			
+			if (targetp.getActiveWeaponInstance() == null)
+			{
+				replyMSG.append("<font color=\"FF0000\">No Weapon!</font><br>");
+			}
+			else
+			{
+				replyMSG.append("<font color=\"FF0000\">Wep Enchant: " + targetp.getActiveWeaponInstance().getEnchantLevel() + "</font><br>");
+			}
+			
+			replyMSG.append("<font color=\"00FF00\">=========>>" + targetp.getName() + "<<=========" + "</font><br>");
+			replyMSG.append("</center></body></html>");
+			
+			adminReply.setHtml(replyMSG.toString());
+			activeChar.sendPacket(adminReply);
+			
+			adminReply = null;
+			targetp = null;
+			replyMSG = null;
+			
+			return true;
+		}
+		else if (command.equalsIgnoreCase("stats"))
+		{
+			if (!Config.ALLOW_SIMPLE_STATS_VIEW)
+			{
+				return false;
+			}
+			
+			if (activeChar.getTarget() == null)
+			{
+				activeChar.sendMessage("You have no one targeted.");
+				return false;
+			}
+			if (activeChar.getTarget() == activeChar)
+			{
+				activeChar.sendMessage("You cannot request your stats.");
+				return false;
+			}
+			
+			if (!(activeChar.getTarget() instanceof L2PcInstance))
+			{
+				activeChar.sendMessage("You can only get the info of a player.");
+				return false;
+			}
+			
+			final L2PcInstance targetp = (L2PcInstance) activeChar.getTarget();
+			
+			if (targetp != null)
+			{
 				NpcHtmlMessage adminReply = new NpcHtmlMessage(5);
-				L2PcInstance targetp = (L2PcInstance) activeChar.getTarget();
 				
-				TextBuilder replyMSG = new TextBuilder("<html><body><center>");
-				replyMSG.append("<br><br><font color=\"00FF00\">=========>>" + targetp.getName() + "<<=========</font><br>");
-				replyMSG.append("<font color=\"FF0000\">Level: " + targetp.getLevel() + "</font><br>");
+				TextBuilder replyMSG = new TextBuilder("<html><body>");
 				
-				if (targetp.getClan() != null)
+				replyMSG.append("<center><font color=\"LEVEL\">[ L2J EVENT ENGINE ]</font></center><br>");
+				replyMSG.append("<br>Statistics for player <font color=\"LEVEL\">" + targetp.getName() + "</font><br>");
+				replyMSG.append("Total kills <font color=\"FF0000\">" + targetp.kills.size() + "</font><br>");
+				replyMSG.append("<br>Detailed list: <br>");
+				
+				for (final String kill : targetp.kills)
 				{
-					replyMSG.append("<font color=\"FF0000\">Clan: " + targetp.getClan().getName() + "</font><br>");
-					replyMSG.append("<font color=\"FF0000\">Alliance: " + targetp.getClan().getAllyName() + "</font><br>");
-				}
-				else
-				{
-					replyMSG.append("<font color=\"FF0000\">Alliance: None</font><br>");
-					replyMSG.append("<font color=\"FF0000\">Clan: None</font><br>");
+					replyMSG.append("<font color=\"FF0000\">" + kill + "</font><br>");
 				}
 				
-				replyMSG.append("<font color=\"FF0000\">Adena: " + targetp.getAdena() + "</font><br>");
-				
-				if (targetp.getInventory().getItemByItemId(6393) == null)
-				{
-					replyMSG.append("<font color=\"FF0000\">Medals : 0</font><br>");
-				}
-				else
-				{
-					replyMSG.append("<font color=\"FF0000\">Medals : " + targetp.getInventory().getItemByItemId(6393).getCount() + "</font><br>");
-				}
-				
-				if (targetp.getInventory().getItemByItemId(3470) == null)
-				{
-					replyMSG.append("<font color=\"FF0000\">Gold Bars : 0</font><br>");
-				}
-				else
-				{
-					replyMSG.append("<font color=\"FF0000\">Gold Bars : " + targetp.getInventory().getItemByItemId(3470).getCount() + "</font><br>");
-				}
-				
-				replyMSG.append("<font color=\"FF0000\">PvP Kills: " + targetp.getPvpKills() + "</font><br>");
-				replyMSG.append("<font color=\"FF0000\">PvP Flags: " + targetp.getPvpFlag() + "</font><br>");
-				replyMSG.append("<font color=\"FF0000\">PK Kills: " + targetp.getPkKills() + "</font><br>");
-				replyMSG.append("<font color=\"FF0000\">HP, CP, MP: " + targetp.getMaxHp() + ", " + targetp.getMaxCp() + ", " + targetp.getMaxMp() + "</font><br>");
-				
-				if (targetp.getActiveWeaponInstance() == null)
-				{
-					replyMSG.append("<font color=\"FF0000\">No Weapon!</font><br>");
-				}
-				else
-				{
-					replyMSG.append("<font color=\"FF0000\">Wep Enchant: " + targetp.getActiveWeaponInstance().getEnchantLevel() + "</font><br>");
-				}
-				
-				replyMSG.append("<font color=\"00FF00\">=========>>" + targetp.getName() + "<<=========" + "</font><br>");
-				replyMSG.append("</center></body></html>");
+				replyMSG.append("</body></html>");
 				
 				adminReply.setHtml(replyMSG.toString());
 				activeChar.sendPacket(adminReply);
 				
 				adminReply = null;
-				targetp = null;
 				replyMSG = null;
 				
 				return true;
 			}
-			case stats:
-			{
-				
-				if (!Config.ALLOW_SIMPLE_STATS_VIEW)
-				{
-					return false;
-				}
-				
-				if (activeChar.getTarget() == null)
-				{
-					activeChar.sendMessage("You have no one targeted.");
-					return false;
-				}
-				if (activeChar.getTarget() == activeChar)
-				{
-					activeChar.sendMessage("You cannot request your stats.");
-					return false;
-				}
-				
-				if (!(activeChar.getTarget() instanceof L2PcInstance))
-				{
-					activeChar.sendMessage("You can only get the info of a player.");
-					return false;
-				}
-				
-				final L2PcInstance targetp = (L2PcInstance) activeChar.getTarget();
-				
-				// L2PcInstance pc = L2World.getInstance().getPlayer(target);
-				
-				if (targetp != null)
-				{
-					NpcHtmlMessage adminReply = new NpcHtmlMessage(5);
-					
-					TextBuilder replyMSG = new TextBuilder("<html><body>");
-					
-					replyMSG.append("<center><font color=\"LEVEL\">[ L2J EVENT ENGINE ]</font></center><br>");
-					replyMSG.append("<br>Statistics for player <font color=\"LEVEL\">" + targetp.getName() + "</font><br>");
-					replyMSG.append("Total kills <font color=\"FF0000\">" + targetp.kills.size() + "</font><br>");
-					replyMSG.append("<br>Detailed list: <br>");
-					
-					for (final String kill : targetp.kills)
-					{
-						replyMSG.append("<font color=\"FF0000\">" + kill + "</font><br>");
-					}
-					
-					replyMSG.append("</body></html>");
-					
-					adminReply.setHtml(replyMSG.toString());
-					activeChar.sendPacket(adminReply);
-					
-					adminReply = null;
-					replyMSG = null;
-					
-					return true;
-				}
-				return false;
-			}
-			default:
-			{
-				return false;
-			}
+			return false;
 		}
 		
+		return false;
 	}
 	
 	@Override
@@ -210,5 +189,4 @@ public class StatsCmd implements IVoicedCommandHandler
 	{
 		return VOICED_COMMANDS;
 	}
-	
 }
