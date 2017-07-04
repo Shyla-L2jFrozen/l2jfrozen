e03b44ba9a7141fbc88afe73abca1ad6a2c05d83
navis83 <navis83@hotmail.it>
2017-07-04 14:15:37 +0000

    Generated Jars have been moved to lib folder

--- gameserver/branches/cleaning/dist/RegisterGameServer.sh
+++ gameserver/branches/cleaning/dist/RegisterGameServer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Djava.util.logging.config.file=console.cfg -cp lib/*:l2jfrozen-login.jar com.l2jfrozen.loginserver.gsregistering.GameServerRegister
+java -Djava.util.logging.config.file=console.cfg -cp lib/*:lib/l2jfrozen-login.jar com.l2jfrozen.loginserver.gsregistering.GameServerRegister
--- gameserver/branches/cleaning/dist/startGameServer.bat
+++ gameserver/branches/cleaning/dist/startGameServer.bat
@@ -5,7 +5,7 @@ mode con: cols=140
 
 REM -------------------------------------
 REM Default parameters for a basic server.
-java -Dfile.encoding=UTF8 -Xms1024m -Xmx1024m -cp ./lib/*;l2jfrozen-game.jar com.l2jfrozen.gameserver.GameServer
+java -Dfile.encoding=UTF8 -Xms1024m -Xmx1024m -cp lib/*;lib/l2jfrozen-game.jar com.l2jfrozen.gameserver.GameServer
 REM
 REM If you have a big server and lots of memory, you could experiment for example with
 REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
