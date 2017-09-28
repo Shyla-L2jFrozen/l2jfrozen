63a7c6af3cbe21638517cbc60700b5c886b1da18
nefer@l2jfrozen.com <nefer@l2jfrozen.com@381c0624-fcce-11e6-9a91-d1aaf0ac2679>
2017-09-28 14:39:03 +0000

    - Update row on db, added also primary key

--- deamon-manager/trunk/src/main/java/com/l2jfrozen/manager/core/Server.java
+++ deamon-manager/trunk/src/main/java/com/l2jfrozen/manager/core/Server.java
@@ -20,6 +20,7 @@ package com.l2jfrozen.manager.core;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.util.GregorianCalendar;
@@ -30,75 +31,91 @@ import com.l2jfrozen.Config;
 import com.l2jfrozen.util.database.CloseUtil;
 import com.l2jfrozen.util.database.L2DatabaseFactory;
 
-public class Server
-{
+public class Server {
 	private static Logger logger = LogManager.getLogManager().getLogger("org.apache.tomcat");
-	private static final String TABLE ="linked_servers";
-	private static final String COLUMN_IDS="RealIp,RealMac,ServerName,LoginServerIp,LoginServerPort,GameServerIp,GameServerPort,ActiveClients,Date";
-	
-	public Server(){
-		
+	private static final String TABLE = "linked_servers";
+	private static final String COLUMN_IDS = "RealIp,RealMac,ServerName,LoginServerIp,LoginServerPort,GameServerIp,GameServerPort,ActiveClients,Date,MaxOnline";
+
+	public Server() {
+
 		logger.info("Creating Service Core");
-		
+
 	}
+
 	/**
 	 * Retained for compatibility with PowerPak
 	 */
-	public int sendInfo(String ServerName,String LoginServerIp,int LoginServerPort, String GameServerIp, int GameServerPort,String networkIPs,int activeClients)
-	{
+	public int sendInfo(String ServerName, String LoginServerIp, int LoginServerPort, String GameServerIp,
+			int GameServerPort, String networkIPs, int activeClients) {
 		logger.info("loading configuration...");
 		Config.loadConfig();
-		logger.info("DATABASE_URL: "+Config.DATABASE_URL);
-		logger.info("DATABASE_LOGIN: "+Config.DATABASE_LOGIN);
-		logger.info("DATABASE_PASSWORD: "+Config.DATABASE_PASSWORD);
-		
+		logger.info("DATABASE_URL: " + Config.DATABASE_URL);
+		logger.info("DATABASE_LOGIN: " + Config.DATABASE_LOGIN);
+		logger.info("DATABASE_PASSWORD: " + Config.DATABASE_PASSWORD);
+
 		String[] networkIP = networkIPs.split(",");
 		logger.info("ReceivedInfo:");
-		logger.info("	RealIp: "+networkIP[0]);
-		logger.info("	RealMac: "+networkIP[1]);
-		logger.info("	ServerName: "+ServerName);
-		logger.info("	LoginServerIp: "+LoginServerIp);
-		logger.info("	LoginServerPort: "+LoginServerPort);
-		logger.info("	GameServerIp: "+GameServerIp);
-		logger.info("	GameServerPort: "+GameServerPort);
-		logger.info("	ActiveClients: "+activeClients);
-		
-		
+		logger.info("	RealIp: " + networkIP[0]);
+		logger.info("	RealMac: " + networkIP[1]);
+		logger.info("	ServerName: " + ServerName);
+		logger.info("	LoginServerIp: " + LoginServerIp);
+		logger.info("	LoginServerPort: " + LoginServerPort);
+		logger.info("	GameServerIp: " + GameServerIp);
+		logger.info("	GameServerPort: " + GameServerPort);
+		logger.info("	ActiveClients: " + activeClients);
+
 		String Date = DateFormat.getDateTimeInstance().format(GregorianCalendar.getInstance().getTime());
-		logger.info("	Date: "+Date);
-		
+		logger.info("	Date: " + Date);
+
 		Connection con = null;
 		PreparedStatement statement = null;
-		try
-		{
+		PreparedStatement statement2 = null;
+
+		try {
 			con = L2DatabaseFactory.getInstance().getConnection(false);
-			statement = con.prepareStatement("INSERT INTO "+TABLE+" ("+COLUMN_IDS+") values (?,?,?,?,?,?,?,?,?)");
+
+			statement2 = con.prepareStatement("SELECT * FROM " + TABLE + " WHERE RealMac='" + networkIP[1] + "'");
+
+			// execute select SQL stetement
+			ResultSet rs = statement2.executeQuery();
+			int OldActiveClients = 0;
+			while (rs.next()) {
+
+				OldActiveClients = rs.getInt("ActiveClients");
+				System.out.println("ActiveClients : " + activeClients);
+				System.out.println("ActiveClients2 : " + OldActiveClients);
+			}
+
+			int MaxOnline = 0;
+			if (activeClients > OldActiveClients)
+				MaxOnline = activeClients;
+			else
+				MaxOnline = OldActiveClients;
+
+			statement = con
+					.prepareStatement("REPLACE INTO " + TABLE + " (" + COLUMN_IDS + ") values (?,?,?,?,?,?,?,?,?,?)");
 			statement.setString(1, networkIP[0]);
 			statement.setString(2, networkIP[1]);
 			statement.setString(3, ServerName);
 			statement.setString(4, LoginServerIp);
-			statement.setString(5, ""+LoginServerPort);
+			statement.setString(5, "" + LoginServerPort);
 			statement.setString(6, GameServerIp);
-			statement.setString(7, ""+GameServerPort);
-			statement.setString(8, ""+activeClients);
+			statement.setString(7, "" + GameServerPort);
+			statement.setString(8, "" + activeClients);
 			statement.setString(9, Date);
+			statement.setInt(10, MaxOnline);
 			statement.executeUpdate();
-			
+
 			statement.close();
 			statement = null;
-			
-		}
-		catch(SQLException e)
-		{
+
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		finally
-		{
+		} finally {
 			CloseUtil.close(con);
 		}
-		
+
 		return 0;
 	}
 
-	
 }
